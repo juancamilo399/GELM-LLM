7534dba93f46650bbd029fd3550862ec144df191
juancamilo399 <juancamilo399@gmail.com>
2024-11-28 16:18:02 -0500

    add validation data to fine tunning

--- fine_tune.py
+++ fine_tune.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+from openai import types
 import os
 from dotenv import load_dotenv
 
@@ -13,15 +14,23 @@ os.environ['OPENAI_API_KEY'] = key
 client = OpenAI()
 
 
-fine_tunning_file = client.files.create(
-  file=open("./data/train.jsonl", "rb"),
+train_fine_tunning_file = client.files.create(
+  file=open("./data/train_es.jsonl", "rb"),
   purpose="fine-tune"
 )
 
-print(f"The file {fine_tunning_file.id} was upload to fine tunning")
+validation_fine_tunning_file = client.files.create(
+  file=open("./data/test_es.jsonl", "rb"),
+  purpose="fine-tune"
+)
+
+print(f"The file {train_fine_tunning_file.id} was upload to train fine tunning")
+
+print(f"The file {validation_fine_tunning_file.id} was upload to validate fine tunning")
 
 fine_tunning_job = client.fine_tuning.jobs.create(
-  training_file=fine_tunning_file.id,
+  training_file=train_fine_tunning_file.id,
+  validation_file=validation_fine_tunning_file.id,
   model="gpt-4o-mini-2024-07-18"
 )
 
