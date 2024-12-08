2fd9f71376a62cea62c8b1fbac45b8061aa58f49
juancamilo399 <juancamilo399@gmail.com>
2024-12-08 18:51:19 -0500

    update readme

--- fine_tune.py
+++ fine_tune.py
@@ -13,6 +13,24 @@ os.environ['OPENAI_API_KEY'] = key
 client = OpenAI()
 
 
-result = client.fine_tuning.jobs.retrieve("ftjob-wiVgjCq0TNQsN5uj1F5xAlnb")
-content = client.files.content("file-KBpMockdayhm1ZRJZQ7mCo")
-print(content)
+train_fine_tunning_file = client.files.create(
+  file=open("./data/train_es.jsonl", "rb"),
+  purpose="fine-tune"
+)
+
+validation_fine_tunning_file = client.files.create(
+  file=open("./data/test_es.jsonl", "rb"),
+  purpose="fine-tune"
+)
+
+print(f"The file {train_fine_tunning_file.id} was upload to train fine tunning")
+
+print(f"The file {validation_fine_tunning_file.id} was upload to validate fine tunning")
+
+fine_tunning_job = client.fine_tuning.jobs.create(
+  training_file=train_fine_tunning_file.id,
+  validation_file=validation_fine_tunning_file.id,
+  model="gpt-4o-mini-2024-07-18"
+)
+
+print(f"The fine tunning job {fine_tunning_job.id} was created")
